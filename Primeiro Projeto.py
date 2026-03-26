2627f2b8141c49936c0137d35fc64e8706f6e360
raphafreitass1982 <raphafreitass1982@gmail.com>
2026-03-26 19:01:57 -0300

    aprendendo a usar o PyChard
    códigos de aprendizagem

---
+++ Primeiro Projeto.py
@@ -0,0 +1,7 @@
+print( "Esse programa exibirá o seu nome completo.")
+nome = input ("coloque seu primeiro nome, por favor: ")
+sobrenome = input ("digite seu sobrenome, pro favor:")
+Nome_completo = nome + " "+ sobrenome
+print( Nome_completo )
+
+
