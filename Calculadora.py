594bcf8991c0725c602153bad4e967f54bb2ed5c
raphafreitass1982 <raphafreitass1982@gmail.com>
2026-04-03 20:20:43 -0300

    calculadora
    Programa de calculadora que resolve as operações básicas de matemática

---
+++ Calculadora.py
@@ -0,0 +1,28 @@
+# calculadora para realizar as 4 operações:
+valor1 : real
+valor2 :  real
+soma : real
+subtração : real
+disisão : real
+
+valor1 = float(int(input("informe o primeiro valor: ")))
+valor2 = float(int(input("informe o segundo valor: " )))
+
+Soma= valor1 + valor2
+
+soma= Soma
+
+Subtração= valor1 - valor2
+
+subtração= Subtração
+
+Multiplicação= valor1 * valor2
+
+multiplicação= Multiplicação
+
+Divisão= valor1 / valor2
+
+divisão= Divisão
+
+print("\nsoma = " ,Soma ,"\nsubtração = "  ,Subtração ,"\nmultiplicação = " ,Multiplicação ,"\ndivisão= " ,Divisão)
+
