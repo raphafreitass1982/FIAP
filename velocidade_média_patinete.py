a649ff93dbd5b28b7ee8da01178718cc97be46d9
raphafreitass1982 <raphafreitass1982@gmail.com>
2026-04-03 22:43:44 -0300

    calculo velocidade média
    calculadora para cálculo de velocidade média, exercício 2

---
+++ velocidade_média_patinete.py
@@ -0,0 +1,16 @@
+# calcular a velocidade média de um patinete:
+#as variáveis utilizadas serão:
+# S -> distância percorrida
+# t -> tempo para percorrer esta distância
+###
+
+S = float(input("distância percorrida (m): "))
+T = float(input("tempo em (s): "))
+
+vm = ( S / T )
+
+print(f"{'vm':<6}= {vm:.2F} m/s")
+
+
+
+
